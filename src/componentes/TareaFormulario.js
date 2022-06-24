ff1644f0a4ef82b39108f0771c236bc229085fa6
ZenWalker <agtecnologia.mex@gmail.com>
2022-06-24 11:56:11 -0700

    modificaciones

---
+++ src/componentes/TareaFormulario.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function TareaFormulario (props) {
+  return (
+    <form className='tarea-formulario'>
+      <input 
+        className='tarea-input' 
+        type='text' 
+        placeholder='Ingrese su Tarea' 
+        name='texto'
+      />
+      <button className='tarea-boton'>
+        Agregar Tarea
+      </button>
+    </form>)
+}
+
+export default TareaFormulario;
